0d022d1d38ed0b06ea78c751ccd9eb91a7e9f67b
Bruce Stull <bruce.stull@gmail.com>
2023-08-31 19:17:26 -0400

    Added 'self_enquiry' and 'base' apps.

--- config/urls.py
+++ config/urls.py
@@ -35,4 +35,6 @@ urlpatterns = [
     
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('self-enquiry/', include('self_enquiry.urls')),
 ]
---
+++ self_enquiry/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path
+
+from self_enquiry.views import JournalListView, JournalDetailView
+from self_enquiry.views import JournalCreateView, JournalUpdateView
+from self_enquiry.views import JournalDeleteView, JournalConfirmDeleteView
+
+
+app_name = "self_enquiry"
+urlpatterns = [
+    path(
+        "create/",
+        JournalCreateView.as_view(),
+        name="create",
+    ),
+    path(
+        "list/",
+        JournalListView.as_view(),
+        name="list",
+    ),
+    path(
+        "<int:pk>/detail/",
+        JournalDetailView.as_view(),
+        name="detail",
+    ),
+    path(
+        "<int:pk>/update/",
+        JournalUpdateView.as_view(),
+        name="update",
+    ),
+    path(
+        "<int:pk>/delete/",
+        JournalDeleteView.as_view(),
+        name="delete",
+    ),
+    path(
+        "<int:pk>/confirm-delete/",
+        JournalConfirmDeleteView.as_view(),
+        name="confirm-delete",
+    ),
+]
