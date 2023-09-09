fd8afe06f99f24839ec574944befad97c0f6d7d1
Bruce Stull <bruce.stull@gmail.com>
2023-09-09 06:48:46 -0400

    In progress

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
     """
     A CustomUser class is added so we can add functionality later. It's more convenient then not to add CustomUser at beginning of project before database migrations are started.
     """
-    # Add additional fields in here and remove `pass` line below.
-    pass
+    registration_accepted = models.BooleanField(
+        "Registration accepted",
+        default=False,
+        help_text="Designates whether this user is accepted for registration.",
+    )
 
     def __str__(self):
         """
